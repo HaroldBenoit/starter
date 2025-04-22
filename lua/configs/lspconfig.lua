b4804d9509dfa98450b5091a0cfb6bf25b744c15
HaroldBenoit <harold_benoit@hotmail.ch>
2025-04-22 11:19:48 +0200

    first changes in config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local servers = {
+  html = {},
+  awk_ls = {},
+  cssls = {},
+}
+
+for name, opts in pairs(servers) do
+  vim.lsp.enable(name)
+  vim.lsp.config(name,opts)
+end
